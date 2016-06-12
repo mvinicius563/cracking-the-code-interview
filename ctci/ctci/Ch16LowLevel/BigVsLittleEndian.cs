1080733f2c60d8fa4dc946d54f9747f40eca6ab1
Marcus Custodio <mvinicius.custodio@gmail.com>
2016-06-12 11:44:00 +0200

    big vs little endian check

---
+++ ctci/ctci/Ch16LowLevel/BigVsLittleEndian.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ctci.Ch16LowLevel
+{
+    /// <summary>
+    /// In big endian, the most significant byte is stored at the memory address location with the lowest address
+    /// This is akin to left-to-right reading order
+    /// Little endian is the reverse: the most significant byte is stored at the address with the highest address
+    /// </summary>
+    internal class BigVsLittleEndian
+    {
+        internal static void Run()
+        {
+            //var x = int.Parse(Console.ReadLine());
+            //Console.WriteLine(Convert.ToString(x, 2));
+            var t = 0x0001;
+            var b = BitConverter.GetBytes(t);
+            if (b[0] == 1)
+            {
+                Console.WriteLine($"LittleEndian; BitConverter.IsLittleEndian={BitConverter.IsLittleEndian}");
+            }
+            else
+            {
+                Console.WriteLine($"BigEndian; BitConverter.IsLittleEndian={BitConverter.IsLittleEndian}");
+            }
+        }
+
+
+    }
+}
--- ctci/ctci/Program.cs
+++ ctci/ctci/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace ctci
 {
+    using Ch16LowLevel;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -10,7 +11,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            BigVsLittleEndian.Run();
         }
     }
 }
